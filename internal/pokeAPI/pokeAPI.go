37e6aa62a40c21ce31b5682c08a57a802226f96a
Tristan Theron <tristantheron@gmail.com>
2024-09-12 19:29:54 +0200

    done

--- internal/pokeAPI/pokeAPI.go
+++ internal/pokeAPI/pokeAPI.go
@@ -2,13 +2,17 @@ package pokeAPI
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	pokecache "github.com/GeminiZA/pokedex/internal/pokeCache"
 )
 
-const locationAPIEndpoint = "https://pokeapi.co/api/v2/location-area/"
+const (
+	locationAPIEndpoint = "https://pokeapi.co/api/v2/location-area/"
+)
 
 type MapRes struct {
 	Count   int     `json:"count"`
@@ -56,4 +60,187 @@ func (api *API) Map(url string) (*MapRes, error) {
 		return nil, err
 	}
 	return mapRes, nil
+}
+
+type ExploreRes struct {
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+}
+
+func parseExloreRes(data []byte) (*ExploreRes, error) {
+	var exploreRes ExploreRes
+	err := json.Unmarshal(data, &exploreRes)
+	if err != nil {
+		return nil, err
+	}
+	return &exploreRes, nil
+}
+
+func (api *API) Explore(location string) (*ExploreRes, error) {
+	if location == "" {
+		return nil, fmt.Errorf("no location to be passed")
+	}
+	endpoint, err := url.JoinPath(locationAPIEndpoint, location)
+	if err != nil {
+		return nil, err
+	}
+	if cachedRes, ok := api.ApiCache.Get(endpoint); ok {
+		return parseExloreRes(cachedRes)
+	}
+	res, err := http.Get(endpoint)
+	if err != nil {
+		return nil, nil
+	}
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	if string(data) == "Not Found" {
+		return nil, fmt.Errorf("area not found")
+	}
+	api.ApiCache.Add(endpoint, data)
+	exploreRes, err := parseExloreRes(data)
+	if err != nil {
+		return nil, err
+	}
+	return exploreRes, nil
+}
+
+type Pokemon struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	BaseExperience int    `json:"base_experience"`
+	Height         int    `json:"height"`
+	IsDefault      bool   `json:"is_default"`
+	Order          int    `json:"order"`
+	Weight         int    `json:"weight"`
+	Abilities      []struct {
+		IsHidden bool `json:"is_hidden"`
+		Slot     int  `json:"slot"`
+		Ability  struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"ability"`
+	} `json:"abilities"`
+	Forms []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"forms"`
+	GameIndices []struct {
+		GameIndex int `json:"game_index"`
+		Version   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version"`
+	} `json:"game_indices"`
+	HeldItems []struct {
+		Item struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"item"`
+		VersionDetails []struct {
+			Rarity  int `json:"rarity"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"held_items"`
+	LocationAreaEncounters string `json:"location_area_encounters"`
+	Moves                  []struct {
+		Move struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"move"`
+		VersionGroupDetails []struct {
+			LevelLearnedAt int `json:"level_learned_at"`
+			VersionGroup   struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version_group"`
+			MoveLearnMethod struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"move_learn_method"`
+		} `json:"version_group_details"`
+	} `json:"moves"`
+	Species struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"species"`
+	Stats []struct {
+		BaseStat int `json:"base_stat"`
+		Effort   int `json:"effort"`
+		Stat     struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
+	PastTypes []struct {
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+		Types []struct {
+			Slot int `json:"slot"`
+			Type struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"type"`
+		} `json:"types"`
+	} `json:"past_types"`
+}
+
+const pokemonAPIEndpoint = "https://pokeapi.co/api/v2/pokemon/"
+
+func parsePokemon(data []byte) (*Pokemon, error) {
+	var pokemon Pokemon
+	err := json.Unmarshal(data, &pokemon)
+	if err != nil {
+		return nil, err
+	}
+	return &pokemon, nil
+}
+
+func (api *API) GetPokemon(name string) (*Pokemon, error) {
+	if name == "" {
+		return nil, fmt.Errorf("no name passed")
+	}
+	endpoint, err := url.JoinPath(pokemonAPIEndpoint, name)
+	if err != nil {
+		return nil, err
+	}
+	if cachedRes, ok := api.ApiCache.Get(endpoint); ok {
+		return parsePokemon(cachedRes)
+	}
+	res, err := http.Get(endpoint)
+	if err != nil {
+		return nil, nil
+	}
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	if string(data) == "Not Found" {
+		return nil, fmt.Errorf("pokemon not found")
+	}
+	api.ApiCache.Add(endpoint, data)
+	pokemon, err := parsePokemon(data)
+	if err != nil {
+		return nil, err
+	}
+	return pokemon, nil
 }
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/GeminiZA/pokedex/internal/pokeAPI"
@@ -13,12 +15,13 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(cfg *config) error
+	callback    func(cfg *config, cmdArgs ...string) error
 }
 
 type config struct {
 	NextMapURL string
 	PrevMapURL string
+	dex        map[string]*pokeAPI.Pokemon
 }
 
 var (
@@ -51,14 +54,33 @@ func initVars() {
 			description: "Display the previous 20 locations",
 			callback:    commandMapB,
 		},
-    "explore": {
-      name: "explote",
-      description: "Display the ",
-    }
+		"explore": {
+			name:        "explore",
+			description: "Display a location",
+			callback:    commandExplore,
+		},
+		"catch": {
+			name:        "catch",
+			description: "Catch a pokemon",
+			callback:    commandCatch,
+		},
+		"inspect": {
+			name:        "inspect",
+			description: "Inspect a pokemon",
+			callback:    commandInspect,
+		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "List your pokemon",
+			callback:    commandPokedex,
+		},
 	}
 }
 
-func commandMap(cfg *config) error {
+func commandMap(cfg *config, cmdArgs ...string) error {
+	if len(cmdArgs) > 0 {
+		return fmt.Errorf("Invalid arguments for map; usage: map")
+	}
 	ret, err := api.Map(cfg.NextMapURL)
 	if err != nil {
 		return err
@@ -75,7 +97,10 @@ func commandMap(cfg *config) error {
 	return nil
 }
 
-func commandMapB(cfg *config) error {
+func commandMapB(cfg *config, cmdArgs ...string) error {
+	if len(cmdArgs) > 0 {
+		return fmt.Errorf("Invalid arguments for mapb; usage: mapb")
+	}
 	ret, err := api.Map(cfg.PrevMapURL)
 	if err != nil {
 		return err
@@ -92,12 +117,91 @@ func commandMapB(cfg *config) error {
 	return nil
 }
 
-func commandExit(cfg *config) error {
+func commandExplore(cfg *config, cmdArgs ...string) error {
+	if len(cmdArgs) != 1 {
+		return fmt.Errorf("Invalid arguments for explore; usage: explore <area_name>")
+	}
+	locationStr := cmdArgs[0]
+	fmt.Println("Exploring", locationStr)
+	exploreRes, err := api.Explore(locationStr)
+	if err != nil {
+		return err
+	}
+	if len(exploreRes.PokemonEncounters) > 0 {
+		fmt.Println("Found Pokemon:")
+		for _, encounter := range exploreRes.PokemonEncounters {
+			fmt.Printf(" - %s\n", encounter.Pokemon.Name)
+		}
+	}
+	return nil
+}
+
+func commandCatch(cfg *config, cmdArgs ...string) error {
+	if len(cmdArgs) != 1 {
+		return fmt.Errorf("Invalid arguments for catch; usage: catch <pokemon>")
+	}
+	nameStr := cmdArgs[0]
+	pokemon, err := api.GetPokemon(nameStr)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Throwing a Pokeball at %s...\n", nameStr)
+	randRes := rand.Int() % 700
+	if randRes > pokemon.BaseExperience {
+		cfg.dex[nameStr] = pokemon
+		fmt.Printf("%s was caught!\n", nameStr)
+	} else {
+		fmt.Printf("%s escaped!\n", nameStr)
+	}
+	return nil
+}
+
+func commandInspect(cfg *config, cmdArgs ...string) error {
+	if len(cmdArgs) != 1 {
+		return fmt.Errorf("Invalid arguments for inspect; usage: inspect <pokemon>")
+	}
+	nameStr := cmdArgs[0]
+	if pokemon, ok := cfg.dex[nameStr]; ok {
+		fmt.Printf("Name: %s\n", pokemon.Name)
+		fmt.Printf("Height: %d\n", pokemon.Height)
+		fmt.Printf("Weight: %d\n", pokemon.Weight)
+		fmt.Printf("Stats:\n")
+		for _, stat := range pokemon.Stats {
+			fmt.Printf("\t-%s: %d\n", stat.Stat.Name, stat.BaseStat)
+		}
+		fmt.Printf("Types:\n")
+		for _, tp := range pokemon.Types {
+			fmt.Printf("\t- %s\n", tp.Type.Name)
+		}
+	} else {
+		fmt.Printf("you have not caught that pokemon\n")
+	}
+	return nil
+}
+
+func commandPokedex(cfg *config, cmdArgs ...string) error {
+	if len(cmdArgs) > 0 {
+		return fmt.Errorf("Invalid arguments for pokedex; usage: pokedex")
+	}
+	fmt.Println("Your Pokedex:")
+	for k := range cfg.dex {
+		fmt.Printf("\t- %s\n", k)
+	}
+	return nil
+}
+
+func commandExit(cfg *config, cmdArgs ...string) error {
+	if len(cmdArgs) > 0 {
+		return fmt.Errorf("Invalid arguments for exit; usage: exit")
+	}
 	defer os.Exit(0)
 	return nil
 }
 
-func commandHelp(cfg *config) error {
+func commandHelp(cfg *config, cmdArgs ...string) error {
+	if len(cmdArgs) > 1 {
+		return fmt.Errorf("Invalid arguments for help; usage: help")
+	}
 	fmt.Print("Welcome to the Pokedex\nUsage:\n\n")
 	for k, v := range commands {
 		fmt.Printf("%s: %s\n", k, v.description)
@@ -111,13 +215,18 @@ func main() {
 	cfg := &config{
 		NextMapURL: "",
 		PrevMapURL: "",
+		dex:        make(map[string]*pokeAPI.Pokemon),
 	}
 	for true {
 		fmt.Print("\nPokedex > ")
 		if scanner.Scan() {
 			line := scanner.Text()
-			if v, ok := commands[line]; ok {
-				v.callback(cfg)
+			commandArgs := strings.Split(line, " ")
+			if v, ok := commands[commandArgs[0]]; ok {
+				err := v.callback(cfg, commandArgs[1:]...)
+				if err != nil {
+					fmt.Println("Error:", err)
+				}
 			} else {
 				fmt.Println("Invalid command:", line)
 				fmt.Println("Use 'help' for more information")
